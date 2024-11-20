38d4afa913ab9045044a33e36e3c5bfd59a99ece
Grätz, Maximilian <maximilian.graetz@interhyp.de>
2024-11-20 11:02:38 +0100

    added routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,9 +9,19 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/pablo',
+    element: <img src='../pablo.png' alt='Pablo' />,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
